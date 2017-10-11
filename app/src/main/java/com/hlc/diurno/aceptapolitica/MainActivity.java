e305d289868993f46bbbee77c5b963c7bda91f64
sergiosanpol <sergiosanpol@gmail.com>
2017-10-11 10:16:32 +0200

    Terminada segunda parte

--- app/src/main/java/com/hlc/diurno/aceptapolitica/MainActivity.java
+++ app/src/main/java/com/hlc/diurno/aceptapolitica/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
 
     private final static int CODIGO_VERIFICA = 1;
 
-    private EditText nombre;
+    private EditText nombre, edad;
     private Button verifica;
     private TextView resultado;
 
@@ -21,24 +21,35 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Asigno las vistas de la activity a las variables
         this.nombre = (EditText) findViewById(R.id.et_nombre);
+        this.edad = (EditText) findViewById(R.id.et_edad);
         this.verifica = (Button) findViewById(R.id.verifica);
         this.resultado = (TextView) findViewById(R.id.resultado);
 
+        //Le asigno al texto que muestra el resultado el texto incial
         this.resultado.setText("Resultado:");
 
+        //Le asigno una escucha al botón verifica que pasará a la activity verifica. Esta nos devolverá unos resultados
         this.verifica.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if(!nombre.getText().toString().isEmpty()){
                     Intent intent = new Intent(view.getContext(), Verifica.class);
                     intent.putExtra("NOMBRE", nombre.getText().toString());
+                    intent.putExtra("EDAD", Integer.parseInt(edad.getText().toString()));
                     startActivityForResult(intent, CODIGO_VERIFICA);
                 }
             }
         });
     }
 
+    /**
+     * Método que nos gestiona los resultados que devuelve la activity Verifica
+     * @param requestCode
+     * @param resultCode
+     * @param data
+     */
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if(requestCode == CODIGO_VERIFICA && resultCode == RESULT_OK){
--- app/src/main/java/com/hlc/diurno/aceptapolitica/Verifica.java
+++ app/src/main/java/com/hlc/diurno/aceptapolitica/Verifica.java
@@ -12,31 +12,42 @@ public class Verifica extends AppCompatActivity {
     private String nombre;
     private TextView saludo;
     private Button acepto, rechazo;
+    private int edad;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_verifica);
 
+        //Asigno las vistas de la activity a las variables
         this.acepto = (Button) findViewById(R.id.acepto);
         this.rechazo = (Button) findViewById(R.id.rechazo);
         this.saludo = (TextView) findViewById(R.id.saludo);
 
-        if(getIntent().hasExtra("NOMBRE")){
+        //Compruebo si la activity anterior a mandado los datos de NOMBRE y EDAD y lo guado en variables. Muestra un mensaje
+        //con el texto de si acepta las politicas
+        if(getIntent().hasExtra("NOMBRE") && getIntent().hasExtra("EDAD")){
             this.nombre = getIntent().getStringExtra("NOMBRE");
-            this.saludo.setText("Hola " + this.nombre + " ¿Aceptas la política de privacidad");
+            this.edad = getIntent().getIntExtra("EDAD", 0);
+            this.saludo.setText("Hola " + this.nombre + " ¿Aceptas la política de privacidad?");
         }
     }
 
+    //La activity tiene dos botones acepto o rechazo. Depende de cual se pulse devuelve una cosa u otra. Si se acepta se comprueba si
+    //es mayor de edad o no
     public void enviarResultado(View v){
         Intent resultados = new Intent();
         if(v.getId() == this.acepto.getId()){
-            resultados.putExtra("RESULTADO", "Aceptada");
+            if(edad >= 18) {
+                resultados.putExtra("RESULTADO", "Aceptada sin restricciones");
+            }else{
+                resultados.putExtra("RESULTADO", "Aceptada con restricciones");
+            }
         }else{
             resultados.putExtra("RESULTADO", "Rechazada");
         }
-        setResult(RESULT_OK, resultados);
-        finish();
+        setResult(RESULT_OK, resultados); //Para devolver resultados a la activity anterior
+        finish(); //Finaliza la activity actual
     }
 
 }
